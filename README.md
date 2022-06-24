216221cf18dc34df736eb6aec049369fbe22f5b0
Aimé Risson <aime.risson.1@gmail.com>
2022-06-24 12:21:20 +0200

    refactoring and update readme file

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # ArchEthic SDK for Python
 This repo aims to provide a Python SDK for the ArchEthic project.<br>
 It is based on the [official javascript sdk](https://github.com/archethic-foundation/libjs).
+## Installation
+Clone this repo with `git clone https://github.com/aime-risson/archethic-python-sdk.git` 
 
+Install requirements with `pip install -r requirements.txt`
+
+Finally, install the package with `pip setup.py install`
+
+## Contribution
+
+Thank you for considering to help out with the source code. 
+We welcome contributions from anyone and are grateful for even the smallest of improvement.
+
+Please to follow this workflow:
+1. Fork it!
+2. Create your feature branch (git checkout -b my-new-feature)
+3. Commit your changes (git commit -am 'Add some feature')
+4. Push to the branch (git push origin my-new-feature)
+5. Create new Pull Request
+
+
+(copied from archethic-foundation)
 ## TODOs
 ### Utils
 - [x] Implementation of utils
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import setuptools
+
+
+VERSION = "0.0.0.1"
+DESCRIPTION = "ArchEthic Python SDK"
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="archethic",
+    version=VERSION,
+    author="Aimé Risson",
+    author_email="aime.risson.1@gmail.fr",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    install_requires=[""],
+    keywords=["python", "crypto", "archethic", "python archethic", "UCO"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Education",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    url="https://github.com/aime-risson/archethic-python-sdk",
+)
