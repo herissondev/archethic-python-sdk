4ccd1d5fc711b65a2f5a9aa097fecff2076082db
Aimé Risson <aime.risson.1@gmail.com>
2022-07-06 10:36:39 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import setuptools
 
 
-VERSION = "0.0.0.4"
+VERSION = "0.0.0.5"
 DESCRIPTION = "ArchEthic Python SDK"
 
 with open("README.md", "r", encoding="utf-8") as fh:
